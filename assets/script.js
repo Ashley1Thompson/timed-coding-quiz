4f70d7bb348784a653e6c894d62be0020108d993
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-06-29 17:47:27 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+const questions = {
+  'what is 1 + 1?': {
+    'A': {
+      1: false
+    },
+    'B': {
+      2: true
+    },
+    'C': {
+      12: false
+    },
+    'D': {
+      22: false
+    },
+  },
+  'what is 2 + 2?': {
+    'A': {
+      1: false
+    },
+    'B': {
+      4: true
+    },
+    'C': {
+      12: false
+    },
+    'D': {
+      22: false
+    },
+  }
+}
