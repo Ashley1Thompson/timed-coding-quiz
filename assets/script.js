202534c72c17a2adbc51194a625fe74bc304b183
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-06-29 22:29:26 -0400

    finish CSS styling, add pseudo code to JS, fix bug with hide class in html

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,7 @@
-const questions = {
-  'what is 1 + 1?': {
-    'A': {
-      1: false
-    },
-    'B': {
-      2: true
-    },
-    'C': {
-      12: false
-    },
-    'D': {
-      22: false
-    },
-  },
-  'what is 2 + 2?': {
-    'A': {
-      1: false
-    },
-    'B': {
-      4: true
-    },
-    'C': {
-      12: false
-    },
-    'D': {
-      22: false
-    },
-  }
-}
+// When I click the start button the game starts
+
+// When I click the start button a timer begins to ocuntdown
+
+// when I answer a question correctly, I am presented with another question
+
+// When I answer a question incorrectly, time is subtracted from the countdown
