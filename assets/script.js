ed8f9145f419a2a5231eefb2dc74312509fe6b38
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-06-29 22:57:11 -0400

    remove next button, add pseudo code to JS

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
-// When I click the start button the game starts
+// When the start button is clicked the game starts
+ function start() {
 
-// When I click the start button a timer begins to ocuntdown
+ }
+// When the start button is clicked a timer begins to countdown
+function finalCountdown() {
 
-// when I answer a question correctly, I am presented with another question
+}
 
-// When I answer a question incorrectly, time is subtracted from the countdown
+// when a questioned is answered correctly, I am presented with another question
+// When a question is answered incorrectly, time is subtracted from the countdown
+function selectAnswer() {
+
+}
+
+// When a question is answered, the result is printed to the bottom of the quiz
+function printResult() {
+
+}
+
+// When the game is finished, a user can view their highscore and input their initials
+
+// The highscore data is stored locally
